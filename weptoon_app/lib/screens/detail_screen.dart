aa3404a140a7ea328adc9b7c248e35adad97f41e
윤형찬 <hyengchan@gmail.com>
2025-07-20 13:37:10 +0900

    :wrench: UI Binding

--- weptoon_app/lib/screens/detail_screen.dart
+++ weptoon_app/lib/screens/detail_screen.dart
@@ -70,6 +70,32 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
             ],
           ),
+          SizedBox(height: 25),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 15),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return Text("...");
+            },
+          ),
         ],
       ),
     );
