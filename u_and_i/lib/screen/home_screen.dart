fb6946c3c1efecbf28d4c40ecf0b32bd649f151f
윤형찬 <72960152+velmash@users.noreply.github.com>
2025-08-19 16:39:45 +0900

    :wrench: uandiConfig

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -1,7 +1,15 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime _selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -11,16 +19,53 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: SizedBox(
           width: MediaQuery.of(context).size.width,
-          child: Column(children: [_Top(), _Bottom()]),
+          child: Column(
+            children: [
+              _Top(selectedDate: _selectedDate, onPressed: onHeartPressed),
+              _Bottom(),
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void onHeartPressed() {
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true, //Dialog 바깥부분 눌러서 hidden
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.center,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              dateOrder: DatePickerDateOrder.ymd,
+              initialDateTime: _selectedDate,
+              maximumDate: DateTime.now(),
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  _selectedDate = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
     );
   }
 }
 
 class _Top extends StatelessWidget {
+  final DateTime selectedDate;
+  final VoidCallback? onPressed;
+  const _Top({super.key, required this.selectedDate, required this.onPressed});
+
   @override
   Widget build(BuildContext context) {
+    final now = DateTime.now();
     final textTheme = Theme.of(context).textTheme;
 
     return Expanded(
@@ -29,14 +74,9 @@ class _Top extends StatelessWidget {
           Text("U&I", style: textTheme.displayLarge),
           Text("우리 처음 만난날", style: textTheme.displayMedium),
 
-          Text("2023-11-23", style: textTheme.bodyMedium),
-          IconButton(
-            onPressed: () {},
-            iconSize: 60,
-            color: Colors.red,
-            icon: Icon(Icons.favorite),
-          ),
-          Text("D+1", style: textTheme.displayMedium),
+          Text("${selectedDate.year}-${selectedDate.month}-${selectedDate.day}", style: textTheme.bodyMedium),
+          IconButton(onPressed: onPressed, iconSize: 60, color: Colors.red, icon: Icon(Icons.favorite)),
+          Text("D+${now.difference(selectedDate).inDays + 1}", style: textTheme.displayMedium),
         ],
       ),
     );
